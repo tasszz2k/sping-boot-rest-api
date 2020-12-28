70a74a5646edf7a28e28e0cfee2bb57fb641d8a7
TASS <48039235+tasszz2k@users.noreply.github.com>
2020-12-29 02:29:28 +0700

    ...

---
+++ src/main/java/com/bananleafnewspaper/api/NewsAPI.java
@@ -0,0 +1,26 @@
+package com.bananleafnewspaper.api;
+
+import com.bananleafnewspaper.dto.NewsDTO;
+import org.springframework.web.bind.annotation.*;
+
+//@RestController = @Controller + @ResponseBody
+@RestController
+public class NewsAPI {
+
+    //@PostMapping = @RequestMapping + method
+    @PostMapping(value = "/news")
+    public NewsDTO createNew(@RequestBody NewsDTO model) {
+        return model;
+    }
+
+    @PutMapping(value = "/news")
+    public NewsDTO updateNews(@RequestBody NewsDTO model){
+        return model;
+    }
+
+    @DeleteMapping(value = "/news")
+    public String deleteNews(@RequestBody int[] ids){
+        //
+        return  "DELETE";
+    }
+}
