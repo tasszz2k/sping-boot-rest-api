02bb5eaa114957cce522625c2db4fd01e9a7a934
TASS <tasszz2k@gmail.com>
2020-12-29 10:00:29 +0700

    Update SpingBootRestApiApplication.java

--- src/main/java/com/bananleafnewspaper/app/SpingBootRestApiApplication.java
+++ src/main/java/com/bananleafnewspaper/app/SpingBootRestApiApplication.java
@@ -1,6 +1,12 @@
+package com.bananleafnewspaper.app;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties
+@EntityScan(basePackages = {"com.bananleafnewspaper.entity"})  // scan JPA entities
 @SpringBootApplication
 public class SpingBootRestApiApplication {
 
